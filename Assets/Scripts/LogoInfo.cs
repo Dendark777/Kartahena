ec56e4c667fc3250c305460fc3bffad7d34c47cf
Savaragon <95469063+Savaragon@users.noreply.github.com>
2022-01-17 16:23:43 +0300

    ReScript + TileSprites

---
+++ Assets/Scripts/LogoInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class LogoInfo : MonoBehaviour
+{
+    public Card selfCard;
+    [SerializeField] public Image logo;
+    
+
+    public void ShowLogoInfo(Card _card)
+    {
+        selfCard = _card;
+        logo.sprite = _card.Logo;
+        logo.preserveAspect = true;
+    }
+}
---
+++ Assets/Scripts/TileManagerScr.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : GameObj
+{
+    public Tile(EnumGameObjValue gameObjValue, string logoPath) : base(gameObjValue, logoPath)
+    {
+    }
+}
+public static class Map
+{
+    public static List<Tile> allTile = new List<Tile>();
+}
+public class TileManagerScr : MonoBehaviour
+{
+
+    public void Awake()
+    {
+        Map.allTile.Add(new Tile(EnumGameObjValue.квадрат, "Sprites/Tile/T_Square"));
+        Map.allTile.Add(new Tile(EnumGameObjValue.круг, "Sprites/Tile/T_Circle"));
+        Map.allTile.Add(new Tile(EnumGameObjValue.треугольник, "Sprites/Tile/T_Triangle"));
+        Map.allTile.Add(new Tile(EnumGameObjValue.ромб, "Sprites/Tile/T_Diamond"));
+        Map.allTile.Add(new Tile(EnumGameObjValue.шестиугольник, "Sprites/Tile/T_Hexagon"));
+        Map.allTile.Add(new Tile(EnumGameObjValue.крест, "Sprites/Tile/T_Xform"));
+
+        Map.allTile.Shuffle();
+
+    }
+}
