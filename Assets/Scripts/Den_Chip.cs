1ba61b008508b6fa8719ed9bf7f726af9db7f232
Savaragon <95469063+Savaragon@users.noreply.github.com>
2022-02-27 14:08:48 +0300

    до изменения Click

---
+++ Assets/Scripts/Den_Chip.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ChipScr : MonoBehaviour, IPointerClickHandler
+{
+    private Color _playerColor;
+    private Image imageChip;
+    private bool _selected;
+    public void ChipInit(Color playerColor)
+    {
+        _playerColor = playerColor;
+        imageChip = GetComponent<Image>();
+        imageChip.color = playerColor;
+        _selected = false;
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChipInit(Color.blue);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left && !_selected)
+        {
+            StartCoroutine(Blink());
+            _selected = true;
+        }
+        else
+        {
+            StopAllCoroutines();
+            imageChip.color = _playerColor;
+            _selected = false;
+        }
+    } 
+    private IEnumerator Blink()
+    {
+        while (true)
+        {
+            if (imageChip.color == _playerColor)
+            {
+                imageChip.color = Color.white;
+            }
+            else
+            {
+                imageChip.color = _playerColor;
+            }
+            yield return new WaitForSeconds(0.4f);
+        }
+    }
+}
