1d30aa4d364ec932c8fcb3853def2e760c357f89
Денис Сироткин <DENdark777@mail.ru>
2022-03-19 20:39:39 +0300

    перед слиянием

---
+++ Assets/Scripts/Dessolve.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dessolve : MonoBehaviour
+{
+    private Material material;
+
+    bool isDissolving = false;
+    float fade = 1f;
+    private void Start()
+    {
+        material = GetComponent<Image>().material;
+    }
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            isDissolving = true;
+        }
+
+        if (isDissolving)
+        {
+            fade -= Time.deltaTime;
+            if (fade <= 0f)
+            {
+                fade = 0f;
+
+            }
+            material.SetFloat("_Fade", fade);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            isDissolving = false;
+            fade = 1f;
+            material.SetFloat("_Fade", fade);
+        }
+    }
+}
