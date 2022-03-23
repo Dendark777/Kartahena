46857815097d7ecaf9d7a2425679b4e43a8af33f
Savaragon <95469063+Savaragon@users.noreply.github.com>
2022-03-23 10:26:48 +0300

    Upd Btn Settings

---
+++ Assets/Btn_CloseSetting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Btn_CloseSetting : MonoBehaviour, IPointerClickHandler
+{
+    
+        [SerializeField] private GameObject PopupSetting;
+        [SerializeField] private List<GameObject> flames;
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            PopupSetting.SetActive(false);
+            ShowFlames();
+        }
+
+        public void ShowFlames()
+        {
+            foreach (var elem in flames)
+            {
+                if (!elem.activeInHierarchy)
+                    elem.SetActive(true);
+            }
+        }
+ }
+
--- Assets/Scripts/BtnSettingsScr.cs
+++ Assets/Scripts/BtnSettingsScr.cs
@@ -6,9 +6,20 @@ using UnityEngine.EventSystems;
 public class BtnSettingsScr : MonoBehaviour,IPointerClickHandler
 {
     [SerializeField] private GameObject PopupSetting;
-
+    [SerializeField] private List<GameObject> flames;
+    
     public void OnPointerClick(PointerEventData eventData)
     {
         PopupSetting.SetActive(true);
+        HideFlames();
+    }
+   
+    public void HideFlames()
+    {
+        foreach(var elem in flames)
+        {
+           if (elem.activeInHierarchy)
+                elem.SetActive(false);
+        }
     }
 }
