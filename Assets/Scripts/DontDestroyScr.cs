bef8eaba06137ccd15080d7386231e656af60b0a
Savaragon <95469063+Savaragon@users.noreply.github.com>
2022-03-24 10:31:55 +0300

    Audio

---
+++ Assets/Scripts/DontDestroyScr.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyScr : MonoBehaviour
+{
+    public static DontDestroyScr instance;
+    private void Start()
+    {
+        if(instance!=null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
