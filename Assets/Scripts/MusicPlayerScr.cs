37c23d38f0663a8ff098c6a4ea9e2e0a32852289
Savaragon <95469063+Savaragon@users.noreply.github.com>
2022-03-24 17:44:27 +0300

    MusicPlayer

---
+++ Assets/Scripts/MusicPlayerScr.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayerScr : MonoBehaviour
+{
+    [SerializeField] private List<AudioClip> clips;
+    [SerializeField] private AudioSource audio;
+    private void Start()
+    {
+        audio = FindObjectOfType<AudioSource>();
+        audio.loop = false;
+    }
+   private AudioClip GetRandomClip()
+    {
+        return clips[Random.Range(0,clips.Count)];
+    }
+
+    private void Update()
+    {
+        if(!audio.isPlaying)
+        {
+            audio.clip = GetRandomClip();
+            audio.Play();
+        }
+    }
+}
